9a3da6fafca81ae61783ea77f42e3a8d3840c9d8
araujobruno0104 <araujobruno0104@gmail.com>
2026-03-09 14:39:32 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "COLE_AQUI_O_LINK_CSV";
+const url = "https://docs.google.com/spreadsheets/d/1Hx3dTYEZOj1SKSN0xcS53rLto4TnaITdWaZW4h3Hqds/edit?usp=drivesdk";
 
 fetch(url)
 .then(res => res.text())
